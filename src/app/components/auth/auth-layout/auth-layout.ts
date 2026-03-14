770d07cba7e050a370c0214f7a3f78c494521645
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2026-03-14 12:54:19 -0300

    refactor: share app header component

--- src/app/components/auth/auth-layout/auth-layout.ts
+++ src/app/components/auth/auth-layout/auth-layout.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AppHeaderComponent } from '../../shared/app-header/app-header';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AppHeaderComponent],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.scss',
 })
--- src/app/pages/items/list-page/list-page.ts
+++ src/app/pages/items/list-page/list-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AppHeaderComponent } from '../../../components/shared/app-header/app-header';
 
 @Component({
   selector: 'app-list-page',
   standalone: true,
-  imports: [],
+  imports: [AppHeaderComponent],
   templateUrl: './list-page.html',
   styleUrl: './list-page.scss',
 })
